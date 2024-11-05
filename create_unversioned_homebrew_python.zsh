96744b38633beb2b6a06308aa889068d16d9c188
mircea <mavram@gmail.com>
2024-11-04 21:44:33 -0500

    python version as a parameter

--- create_unversioned_homebrew_python.zsh
+++ create_unversioned_homebrew_python.zsh
@@ -1,13 +1,16 @@
 #!/bin/zsh
 
+# Set the default version if not provided
+PYTHON_VERSION=${1:-3.12}
+
 # Directory where Homebrew installs Python
-PYTHON_HOME=$(brew --prefix python@3.12)/bin
+PYTHON_HOME=$(brew --prefix python@$PYTHON_VERSION)/bin
 
 # Destination directory for the symbolic links
 LINK_DIR="/usr/local/bin"
 
 # Create unversioned symlinks for python, pip, etc.
-ln -sf ${PYTHON_HOME}/python3.12 ${LINK_DIR}/python
-ln -sf ${PYTHON_HOME}/pip3.12 ${LINK_DIR}/pip
+ln -sf ${PYTHON_HOME}/python${PYTHON_VERSION} ${LINK_DIR}/python
+ln -sf ${PYTHON_HOME}/pip${PYTHON_VERSION} ${LINK_DIR}/pip
 
 # Add similar lines for other executables like pydoc3, etc., if needed
