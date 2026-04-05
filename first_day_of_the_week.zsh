2f23672f0a45acc0b3abe2173ed244f9ccf87f8c
mircea <mavram@gmail.com>
2026-04-05 16:48:56 -0400

    updated script to use defaults and ISO

--- first_day_of_the_week.zsh
+++ first_day_of_the_week.zsh
@@ -1,15 +1,22 @@
 #!/bin/zsh
 
-# Usage: ./first_day_of_week.zsh <year> <week_number>
+# Usage: ./first_day_of_week.zsh [year] [week_number]
 
-year=$1
-week_number=$2
+# Default to current ISO year and week
+current_iso_year=$(date +%G)
+current_iso_week=$(date +%V)
 
-# macOS `date` format for ISO-8601 first day of the week calculation
-start_of_year=$(date -jf "%Y-%m-%d" "${year}-01-01" +"%Y-%m-%d")
-day_num=$(date -jf "%Y-%m-%d" "$start_of_year" +"%u")
-day_diff=$(( (7 + 1 - day_num) % 7 )) # days to next Monday
-first_monday=$(date -j -v+${day_diff}d -f "%Y-%m-%d" "$start_of_year" +"%Y-%m-%d")
+year=${1:-$current_iso_year}
+week_number=${2:-$current_iso_week}
+
+# Find Jan 4th (guaranteed to be in ISO week 1)
+jan4="${year}-01-04"
+
+# Find the Monday of ISO week 1
+day_num=$(date -jf "%Y-%m-%d" "$jan4" +"%u")  # 1=Mon ... 7=Sun
+first_monday=$(date -j -v-"$((day_num - 1))"d -f "%Y-%m-%d" "$jan4" +"%Y-%m-%d")
+
+# Add (week_number - 1) weeks
 first_day=$(date -j -v+"$(( (week_number - 1) * 7 ))"d -f "%Y-%m-%d" "$first_monday" +"%Y-%m-%d")
 
-echo "The first day of week number $week_number in $year is $first_day"
+echo "The first day of ISO week $week_number in $year is $first_day"
