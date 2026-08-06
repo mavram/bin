0c436a46261d6fd473704f216dc9082a345f6fc2
mircea <mavram@gmail.com>
2026-08-06 00:07:14 -0400

    context size and token limit

--- run_model.zsh
+++ run_model.zsh
@@ -1,6 +1,15 @@
 #!/usr/bin/env zsh
 
 model="${1:-qwen3.6:35b-mlx}"
+context_window="${2:-262144}"
+auto_compact_limit="${3:-229376}"
+
+(( $# > 0 )) && shift
+(( $# > 0 )) && shift
 (( $# > 0 )) && shift
 
-exec ollama launch codex --model "$model" -- --disable apps "$@"
+exec ollama launch codex --model "$model" -- \
+  --disable apps \
+  -c "model_context_window=$context_window" \
+  -c "model_auto_compact_token_limit=$auto_compact_limit" \
+  "$@"
